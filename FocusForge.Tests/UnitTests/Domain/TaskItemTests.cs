2d02c2e7c0319479e35b76c584e274c90ca02107
Max Allan-Smith <max.allan-smith@mit-tech.co.uk>
2025-07-22 22:18:04 +0100

    Added test library and included a starting test

---
+++ FocusForge.Tests/UnitTests/Domain/TaskItemTests.cs
@@ -0,0 +1,24 @@
+﻿namespace FocusForge.Tests.UnitTests.Domain
+{
+    public class TaskItemTests
+    {
+        [Test]
+        public void CanCreateAndCompleteTask()
+        {
+            var task = new TaskItem("Write test plan");
+
+            Assert.AreEqual("Write test plan", task.Title);
+            Assert.IsFalse(task.IsCompleted);
+
+            task.MarkComplete();
+
+            Assert.IsTrue(task.IsCompleted);
+        }
+
+        [Test]
+        public void ThrowsIfTitleIsEmpty()
+        {
+            Assert.Throws<ArgumentException>(() => new TaskItem(""));
+        }
+    }
+}
